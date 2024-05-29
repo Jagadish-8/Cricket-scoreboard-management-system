ac8fe13f0c07d44d94eb0415eb2ab0d07954dc9c
Alok <karnaalok48@gmail.com>
2024-05-30 01:29:14 +0545

    added static files and defined new urls and views

--- Cricket_scoreboard_management_system/scoreboard/urls.py
+++ Cricket_scoreboard_management_system/scoreboard/urls.py
@@ -4,5 +4,10 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomePage().index, name = 'index'),
+    path('', views.Pages().index, name = 'index'),
+    path('news_updates/', views.Pages().newsandupdates, name='newsandupdates'),
+    path('about/', views.Pages().about, name='about'),
+    path('contact/', views.Pages().contact, name='contact'),
+    path('help/', views.Pages().help, name='help'),
+
 ]
--- Cricket_scoreboard_management_system/scoreboard/views.py
+++ Cricket_scoreboard_management_system/scoreboard/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-class HomePage:
+class Pages:
     def index(self, request):
         return render(request, 'index.html')
+    
+    def newsandupdates(self, request):
+        return HttpResponse("News and updates page")
+    
+    def about(self, request):
+        return HttpResponse("About page called")
+    
+    def contact(self, request):
+        return HttpResponse("Contact page called")
+    
+    def help(self, request):
+        return HttpResponse("Help page called")
