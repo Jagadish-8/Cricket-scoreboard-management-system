aca121cafbb4a227358e382dd5c5d52106685da4
Alok <karnaalok48@gmail.com>
2024-06-02 00:54:23 +0545

    added fields in models.py

--- Cricket_scoreboard_management_system/scoreboard/models.py
+++ Cricket_scoreboard_management_system/scoreboard/models.py
@@ -3,15 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Match(models.Model):
-    match_number = models.PositiveSmallIntegerField(unique = True, default=1, blank = False, null = False)
+    match_number = models.PositiveSmallIntegerField(unique = True, default=1, blank = False, null = True)
     match_date = models.DateField(auto_now_add = True, blank = False, null = False)
-    match_location = models.CharField(max_length = 15, blank = False, null = False)
+    match_location = models.CharField(max_length = 15, blank = False, null = True)
 
 class Team(models.Model):
+    team_number = [
+        (1, 'Team 1'),
+        (2, 'Team 2'),
+    ]
     match_number = models.ForeignKey(Match, on_delete = models.CASCADE, blank = False, null = False, default = 1)
     team_name = models.CharField(max_length =20, blank = False, null = False)
     team_coach = models.CharField(max_length = 10, blank = False, null = False)
     team_homeground = models.CharField(max_length = 15, blank = True, null = True)
+    team_number = models.IntegerField(default = 1, choices = team_number)
 
 class Player(models.Model):
     role = [
@@ -29,7 +34,7 @@ class Player(models.Model):
 class Score(models.Model):
     player = models.ForeignKey(Player, on_delete = models.CASCADE)
     match_number = models.ForeignKey(Match, on_delete = models.CASCADE, blank = False, null = False, default = 1)
-    score = models.PositiveIntegerField()
+    team_number = models.ForeignKey(Team, default = 1, on_delete=models.CASCADE)
     run = models.PositiveIntegerField()
     wickets = models.PositiveIntegerField()
     overs = models.PositiveIntegerField()
